01d35be64134bbe68674490de591aa717f335830
nyangry <iacta.alea.esto@gmail.com>
2014-11-17 15:31:10 +0900

    Brewfileはもう古そうなのでシェル化

---
+++ Brewfile.sh
@@ -0,0 +1,89 @@
+brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+
+
+brew tap sanemat/font
+brew tap homebrew/versions
+#brew tap phinze/homebrew-cask
+brew tap peco/peco
+
+#brew install ricty --vim-powerline
+#brew install brew-cask
+brew install caskroom/cask/brew-cask
+
+brew install git
+brew install openssl
+brew install readline
+brew install tree
+brew install nkf
+brew install the_silver_searcher
+brew install jq
+brew install peco
+
+brew install zsh
+brew install reattach-to-user-namespace
+brew install ssh-copy-id
+brew install tmux
+brew install vim --with-lua, --override-system-vi
+#brew install macvim
+
+brew install rbenv
+brew install ruby-build
+brew install lua
+brew install ctags
+
+brew install mysql
+#brew install mongodb
+brew install node
+brew install redis
+
+brew install imagemagick
+brew install gibo
+
+
+brew tap caskroom/versions
+
+#brew priority
+brew cask install google-chrome
+brew cask install bettertouchtool
+brew cask install sequel-pro
+brew cask install imageoptim
+brew cask install ccleaner
+brew cask install alfred
+brew cask install sublime-text3
+brew cask install clipmenu
+#brew cask install mailplane
+brew cask install anvil
+brew cask install iterm2
+brew cask install appcleaner
+#brew cask install p4merge
+#brew cask install google-japanese-ime
+brew cask install dash
+brew cask install tower
+brew cask install balsamiq-mockups
+brew cask install transmit
+brew cask install macdown
+#brew cask install difffork
+brew cask install diffmerge
+brew cask install virtualbox
+#brew cask install vagrant
+brew cask install yorufukurou
+
+
+#brew normal
+brew cask install sequential
+brew cask install silverlight
+brew cask install insomniax
+brew cask install karabiner
+#brew cask install bitcasa
+#brew cask install Wondershare Player
+#brew cask install Stackroom
+#brew cask install calibre
+#brew cask install PhoneClean
+#brew cask install all2mp3
+#brew cask install Mapture-0.0.16
+
+
+
+brew cask alfred link
+
+brew cleanup && brew cask cleanup
