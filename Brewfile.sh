13740da11b87f3d17ce3cc8fcf9585b77d35c613
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2015-03-16 08:50:44 +0900

    update brewfile

--- Brewfile.sh
+++ Brewfile.sh
@@ -1,4 +1,5 @@
-brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+# brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+brew update brew cleanup
 
 
 # brew tap sanemat/font
@@ -8,10 +9,6 @@ brew tap peco/peco
 
 #brew install ricty --vim-powerline
 #brew install brew-cask
-brew install phinze/cask/brew-cask
-brew install caskroom/cask/brew-cask
-
-brew install rbenv-gem-rehash
 
 brew install git
 brew install openssl
@@ -29,8 +26,6 @@ brew install tmux
 brew install vim --with-lua --override-system-vi
 #brew install macvim
 
-brew install rbenv
-brew install ruby-build
 brew install lua
 brew install ctags
 
@@ -43,49 +38,52 @@ brew install imagemagick
 brew install gibo
 
 
-brew tap caskroom/versions
+# brew install phinze/cask/brew-cask
+# brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
+#
+# #brew priority
+# brew cask install google-chrome
+# brew cask install bettertouchtool
+# brew cask install sequel-pro
+# brew cask install imageoptim
+# brew cask install ccleaner
+# brew cask install alfred
+# brew cask install sublime-text3
+# brew cask install clipmenu
+# #brew cask install mailplane
+# # brew cask install anvil
+# brew cask install iterm2-nightly
+# brew cask install appcleaner
+# #brew cask install p4merge
+# #brew cask install google-japanese-ime
+# brew cask install dash
+# brew cask install tower
+# brew cask install balsamiq-mockups
+# brew cask install transmit
+# brew cask install macdown
+# #brew cask install difffork
+# brew cask install diffmerge
+# brew cask install virtualbox
+# #brew cask install vagrant
+# brew cask install yorufukurou
+#
+#
+# #brew normal
+# brew cask install sequential
+# brew cask install silverlight
+# brew cask install insomniax
+# brew cask install karabiner
+# #brew cask install Wondershare Player
+# #brew cask install Stackroom
+# #brew cask install calibre
+# #brew cask install PhoneClean
+# #brew cask install all2mp3
+# #brew cask install Mapture-0.0.16
+#
+#
+#
+# brew cask alfred link
 
-#brew priority
-brew cask install google-chrome
-brew cask install bettertouchtool
-brew cask install sequel-pro
-brew cask install imageoptim
-brew cask install ccleaner
-brew cask install alfred
-brew cask install sublime-text3
-brew cask install clipmenu
-#brew cask install mailplane
-# brew cask install anvil
-brew cask install iterm2-nightly
-brew cask install appcleaner
-#brew cask install p4merge
-#brew cask install google-japanese-ime
-brew cask install dash
-brew cask install tower
-brew cask install balsamiq-mockups
-brew cask install transmit
-brew cask install macdown
-#brew cask install difffork
-brew cask install diffmerge
-brew cask install virtualbox
-#brew cask install vagrant
-brew cask install yorufukurou
-
-
-#brew normal
-brew cask install sequential
-brew cask install silverlight
-brew cask install insomniax
-brew cask install karabiner
-#brew cask install Wondershare Player
-#brew cask install Stackroom
-#brew cask install calibre
-#brew cask install PhoneClean
-#brew cask install all2mp3
-#brew cask install Mapture-0.0.16
-
-
-
-brew cask alfred link
-
-brew cleanup && brew cask cleanup
+# brew cleanup && brew cask cleanup
+brew cleanup
