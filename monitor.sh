631443ab469efdf53cca16e13ddfb5a9e134a71d
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2016-08-31 16:32:35 +0900

    Fix monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,7 +3,7 @@ tmux split-window -v -t monitor
 tmux split-window -h -t monitor.1
 tmux split-window -h -t monitor.2
 
-tmux send-keys -t monitor.1 'watch -n 5 "pstree -U |grep -E \"(sql|vim|ruby|rails|spring|pa_)\" | grep -v grep"' C-m
+tmux send-keys -t monitor.1 'watch -n 5 "pstree -U |grep -E \"(mysql|vim|ruby|rails|spring|pa_|redis|sidekiq)\" | grep -v grep"' C-m
 tmux send-keys -t monitor.3 'htop' C-m
 tmux send-keys -t monitor.4 'watch -n 5 "tmux list-sessions"' C-m
 
