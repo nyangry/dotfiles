02df9d4a5488689b59cb165c997a130d75330be8
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2016-12-01 17:53:24 +0900

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,8 +3,8 @@ tmux split-window -v -t monitor
 tmux split-window -h -t monitor.1
 tmux split-window -h -t monitor.3
 
-tmux send-keys -t monitor.1 'watch -n 5 "pstree -U |grep -E \"(mysql|vim|ruby|rails|spring|pa_|redis|sidekiq)\" | grep -v grep"' C-m
+tmux send-keys -t monitor.1 'watch -n 3 "pstree -U |grep -E \"(mysql|vim|ruby|rails|spring|pa_|my_|redis|sidekiq)\" | grep -v grep"' C-m
 tmux send-keys -t monitor.2 'htop' C-m
-tmux send-keys -t monitor.4 'watch -n 5 "tmux list-sessions"' C-m
+tmux send-keys -t monitor.4 'watch -n 3 "tmux list-sessions"' C-m
 
 tmux select-pane -t monitor.3
