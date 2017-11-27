7f775227830e20837e7fc22ff687d9bb0a6706a1
nyangry <iacta.alea.esto@gmail.com>
2017-11-27 09:21:48 +0900

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,7 +4,7 @@ tmux split-window -h -t monitor.1
 tmux split-window -h -t monitor.3
 
 tmux send-keys -t monitor.1 'watch -n 3 "pstree -U |grep -E \"(mysql|vim|ruby|rails|pa_|my_|redis|sidekiq|delayed_job|ssh)\" | grep -v grep"' C-m
-tmux send-keys -t monitor.2 'htop' C-m
+tmux send-keys -t monitor.2 '' C-m
 tmux send-keys -t monitor.4 'watch -n 3 "tmux list-sessions"' C-m
 
 tmux select-pane -t monitor.3
