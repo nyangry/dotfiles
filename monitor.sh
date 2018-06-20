819103fd88195345aa6140a0bce3b6bb0905728b
nyangry <iacta.alea.esto@gmail.com>
2018-06-20 10:39:10 +0900

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,10 @@
 tmux rename-window 'monitor'
-tmux split-window -v -t monitor
-tmux split-window -h -t monitor.1
-tmux split-window -h -t monitor.3
+tmux split-window -h -t monitor
+tmux split-window -v -t monitor.2
+tmux split-window -v -t monitor.3
 
-tmux send-keys -t monitor.1 'watch -n 3 "pstree -U |grep -E \"(mysql|vim|ruby|rails|pa_|my_|redis|sidekiq|delayed_job|ssh)\" | grep -v grep"' C-m
-tmux send-keys -t monitor.2 '' C-m
-tmux send-keys -t monitor.4 'watch -n 3 "tmux list-sessions"' C-m
+tmux send-keys -t monitor.1 'watch -n 3 "pstree -U |grep -E \"(mysql|vim|ruby|rails|puma|pa_|my_|spring|redis|sidekiq|delayed_job|ssh)\" | grep -v -e grep -e fsevent"' C-m
+tmux send-keys -t monitor.2 'htop' C-m
+tmux send-keys -t monitor.3 'watch -n 3 "tmux list-sessions"' C-m
 
-tmux select-pane -t monitor.3
+tmux select-pane -t monitor.4
