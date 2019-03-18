97c1756c163159868302cabd68913b391220d6aa
nyangry <iacta.alea.esto@gmail.com>
2019-03-18 13:11:21 +0900

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,7 +3,7 @@ tmux split-window -h -t monitor
 tmux split-window -v -t monitor.2
 tmux split-window -v -t monitor.3
 
-tmux send-keys -t monitor.1 'watch -n 3 "pstree -U |grep -E \"(vim|nvim|ruby|rails|puma|pa_|my_|spring|mysql|redis|sidekiq|delayed_job|ssh)\" | grep -v -e grep -e fsevent"' C-m
+tmux send-keys -t monitor.1 'watch -n 3 "ps -eo 'pid, %cpu, %mem, start, etime, command' | grep -E \"(vim|nvim|ruby|rails|puma|pa_|my_|spring|mysql|redis|sidekiq|delayed_job)\" | grep -v -e grep -e fsevent -e start_host | sort -b -f -k 6r,6r -k1""' C-m
 tmux send-keys -t monitor.2 'htop' C-m
 tmux send-keys -t monitor.3 'watch -n 3 "tmux list-sessions"' C-m
 
