bf96e570f290a29904a869b98a595935ceb0a98b
nyangry <iacta.alea.esto@gmail.com>
2019-07-06 21:36:14 +0900

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,13 @@
 tmux rename-window 'monitor'
 tmux split-window -h -t monitor
-tmux split-window -v -t monitor.2
+tmux split-window -v -t monitor.1
 tmux split-window -v -t monitor.3
 
+tmux resize-pane -D -t monitor.2 15
+tmux resize-pane -D -t monitor.3 15
+
 tmux send-keys -t monitor.1 'watch -n 3 "ps -eo 'pid, %cpu, %mem, start, etime, command' | grep -E \"(vim|nvim|ruby|rails|puma|pa_|my_|spring|mysql|redis|sidekiq|delayed_job)\" | grep -v -e grep -e fsevent -e start_host -e .cache | sort -b -f -k 6r,6r -k1"' C-m
-tmux send-keys -t monitor.2 'htop -s PERCENT_CPU' C-m
-tmux send-keys -t monitor.3 'watch -n 3 "tmux list-sessions"' C-m
+tmux send-keys -t monitor.2 'watch -n 3 "tmux list-sessions"' C-m
+tmux send-keys -t monitor.3 'htop' C-m
 
 tmux select-pane -t monitor.4
