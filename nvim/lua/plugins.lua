0bed8002f5a3efecb1a360e1d66ad444dc6f0149
nyangry <iacta.alea.esto@gmail.com>
2024-04-24 15:26:12 +0900

    Fix rg option for telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -105,7 +105,7 @@ return {
       vim.keymap.set('n', '<C-f>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
       vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+      vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
     end
   },
@@ -126,7 +126,6 @@ return {
             "--line-number",
             "--column",
             "--smart-case",
-            "-uu",
           },
         },
         extensions = {
