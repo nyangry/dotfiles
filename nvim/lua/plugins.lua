da14c40064b59f92bb4b30dd4a695976c813955f
nyangry <iacta.alea.esto@gmail.com>
2024-04-26 08:51:45 +0900

    Update lualine setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -36,7 +36,17 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     config = function()
-      require('lualine').setup()
+      require('lualine').setup({
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 3 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+          }
+        }
+      })
     end
   },
 
