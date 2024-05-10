a69426a2378d8a46cd3071ad74d0465d16cb7518
nyangry <iacta.alea.esto@gmail.com>
2024-05-10 13:38:18 +0900

    Update nvim plugin setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -72,7 +72,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+        ensure_installed = { "python", "lua", "vim", "sql", "graphql", "json", "yaml", "javascript", "html" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
