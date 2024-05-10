fd52157bb928da11405342c82ac557ced521b808
nyangry <iacta.alea.esto@gmail.com>
2024-05-10 09:27:37 +0900

    Update nvim plugin setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -347,7 +347,39 @@ return {
     'echasnovski/mini.comment',
     version = '*',
     config = function ()
-      require('mini.comment').setup()
+      require('mini.comment').setup({
+        -- Options which control module behavior
+        options = {
+          -- Function to compute custom 'commentstring' (optional)
+          custom_commentstring = nil,
+
+          -- Whether to ignore blank lines when commenting
+          ignore_blank_line = false,
+
+          -- Whether to recognize as comment only lines without indent
+          start_of_line = false,
+
+          -- Whether to force single space inner padding for comment parts
+          pad_comment_parts = true,
+        },
+
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          -- Toggle comment (like `gcip` - comment inner paragraph) for both
+          -- Normal and Visual modes
+          comment = '--',
+
+          -- Toggle comment on current line
+          comment_line = '--',
+
+          -- Toggle comment on visual selection
+          comment_visual = '--',
+
+          -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+          -- Works also in Visual mode if mapping differs from `comment_visual`
+          textobject = '--',
+        },
+      })
     end
   },
 
