dd8e88d39fe0496ca73125c0618bbeb7115a20c5
nyangry <iacta.alea.esto@gmail.com>
2024-05-10 09:28:15 +0900

    Remove unused nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -434,22 +434,6 @@ return {
     end
   },
 
-
-  -- lint
-  -- {
-  --   'mfussenegger/nvim-lint',
-  --   config = function()
-  --     require('lint').linters_by_ft = {
-  --       python = {'pylint',}
-  --     }
-  --     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  --       callback = function()
-  --         require("lint").try_lint()
-  --       end,
-  --     })
-  --   end
-  -- },
-
   -- LSP
   -- {
   --   'neovim/nvim-lspconfig',
