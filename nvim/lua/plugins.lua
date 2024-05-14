94303ecf83d5c012188745bd3e24855f388c6358
nyangry <iacta.alea.esto@gmail.com>
2024-05-14 12:54:26 +0900

    Add nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -119,6 +119,31 @@ return {
     end
   },
 
+  -- code outline window
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      local aerial = require("aerial")
+
+      aerial.setup({
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        end,
+      })
+      -- You probably also want to set a keymap to toggle aerial
+      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    end
+  },
+
   -- fuzzy finder
   {
     'nvim-telescope/telescope.nvim',
