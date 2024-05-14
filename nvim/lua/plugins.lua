de6845e8fe259bf64a648343b244bac079e81604
nyangry <iacta.alea.esto@gmail.com>
2024-05-14 13:21:09 +0900

    Update nvim plugin setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -249,7 +249,10 @@ return {
         },
         window = {
           -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
+          documentation =  {
+            max_width = 0,
+            max_height = 0
+          }
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
