2501b72bbe9272c2a013522feb7e925bd412bcfa
nyangry <iacta.alea.esto@gmail.com>
2024-05-14 10:22:56 +0900

    Tweak

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -504,7 +504,6 @@ return {
           },
         }
       })
-      local opts = { noremap = true, silent = true }
       vim.keymap.set("n", "<C-n>", ":lua require('neogen').generate()<CR>", opts)
       -- vim.keymap.set("n", "<Leader>nf", ":lua require('neogen').generate({ type = 'func' })<CR>", opts)
     end
