1b7e5f2cfc60a4f8690d4dd4b58d3b0d04eac7ec
nyangry <iacta.alea.esto@gmail.com>
2024-05-14 12:54:22 +0900

    Update nvim plugin setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -641,14 +641,14 @@ return {
 
             -- format on save
             vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-            vim.api.nvim_create_autocmd(event, {
-              buffer = bufnr,
-              group = group,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr, async = async })
-              end,
-              desc = "[lsp] format on save",
-            })
+            -- vim.api.nvim_create_autocmd(event, {
+            --   buffer = bufnr,
+            --   group = group,
+            --   callback = function()
+            --     vim.lsp.buf.format({ bufnr = bufnr, async = async })
+            --   end,
+            --   desc = "[lsp] format on save",
+            -- })
           end
 
           if client.supports_method("textDocument/rangeFormatting") then
