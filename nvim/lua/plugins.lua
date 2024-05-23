814320f5411da6822658385caea13afcd60bd989
nyangry <iacta.alea.esto@gmail.com>
2024-05-24 08:47:53 +0900

    Update nvim plugin setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -791,18 +791,18 @@ return {
             end, { buffer = bufnr, desc = "[lsp] format" })
 
             -- format on save
-            -- local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-            -- local event = "BufWritePre" -- or "BufWritePost"
-            -- local async = event == "BufWritePre"
-            -- vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-            -- vim.api.nvim_create_autocmd(event, {
-            --   buffer = bufnr,
-            --   group = group,
-            --   callback = function()
-            --     vim.lsp.buf.format({ bufnr = bufnr, async = async })
-            --   end,
-            --   desc = "[lsp] format on save",
-            -- })
+            local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+            local event = "BufWritePre" -- or "BufWritePost"
+            local async = event == "BufWritePre"
+            vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+            vim.api.nvim_create_autocmd(event, {
+              buffer = bufnr,
+              group = group,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr, async = async })
+              end,
+              desc = "[lsp] format on save",
+            })
           end
 
           if client.supports_method("textDocument/rangeFormatting") then
