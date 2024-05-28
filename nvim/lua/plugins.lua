6a1eeec7745d8e2df1f61c00352983d26ee66244
nyangry <iacta.alea.esto@gmail.com>
2024-05-28 13:49:58 +0900

    Add treesitter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -103,7 +103,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = { "python", "lua", "vim", "sql", "graphql", "json", "yaml", "javascript", "html" },
+        ensure_installed = { "python", "kotlin", "ruby", "lua", "vim", "sql", "graphql", "json", "yaml", "javascript", "html" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
