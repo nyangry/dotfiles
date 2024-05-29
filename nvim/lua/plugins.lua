fd87167e42723ea236f893c4ac9964f85f70506d
nyangry <iacta.alea.esto@gmail.com>
2024-05-29 20:21:17 +0900

    Update nvim plugin setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -181,6 +181,19 @@ return {
     tag = '0.1.4',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close,
+            },
+          },
+        },
+      })
+
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<C-f>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
