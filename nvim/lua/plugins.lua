0d1c8a177260863802ef1fc8dc908dae39dd8094
nyangry <iacta.alea.esto@gmail.com>
2024-06-07 03:59:54 +0900

    Add nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -400,6 +400,59 @@ return {
     end
   },
 
+  -- git sign
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      local gitsigns = require('gitsigns')
+      gitsigns.setup {
+        signs = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+        watch_gitdir = {
+          follow_files = true
+        },
+        auto_attach = true,
+        attach_to_untracked = true,
+        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 500,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+        current_line_blame_formatter_opts = {
+          relative_time = false,
+        },
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil, -- Use default
+        max_file_length = 40000, -- Disable if file is longer than this (in lines)
+        preview_config = {
+          -- Options passed to nvim_open_win
+          border = 'single',
+          style = 'minimal',
+          relative = 'cursor',
+          row = 0,
+          col = 1
+        },
+      }
+      keymap('n', '<leader>bf', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>', opts)
+      keymap('n', '<leader>b', '<cmd>lua require"gitsigns".blame_line()<CR>', opts)
+    end
+  },
+
   -- removes trailing white space and empty lines on BufWritePre
   {
     "mcauley-penney/tidy.nvim",
