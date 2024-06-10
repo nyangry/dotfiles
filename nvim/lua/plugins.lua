cb67a4e4a8d5dc171108af58518428410119ac29
nyangry <iacta.alea.esto@gmail.com>
2024-06-10 20:30:03 +0900

    Remove unnecessary code

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,24 +21,6 @@ local keymap = vim.api.nvim_set_keymap
 
 return {
   -- colorscheme
-  --  { 'shaunsingh/nord.nvim',
-  --    lazy = false,
-  --    priority = 1000,
-  --    config = function()
-  --      vim.cmd[[colorscheme nord]]
-  --
-  --      -- Example config in lua
-  --      vim.g.nord_contrast = true
-  --      vim.g.nord_borders = false
-  --      vim.g.nord_disable_background = true
-  --      vim.g.nord_italic = false
-  --      vim.g.nord_uniform_diff_background = true
-  --      vim.g.nord_bold = false
-  --
-  --      -- Load the colorscheme
-  --      require('nord').set()
-  --    end
-  --  },
   {
     'EdenEast/nightfox.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
