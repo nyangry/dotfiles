9cf8b7e4b1dec43d19562e7e552f9cd67aef22e0
nyangry <iacta.alea.esto@gmail.com>
2024-06-18 19:16:51 +0900

    Update nvim plugin setting for completion

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -332,7 +332,7 @@ return {
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
@@ -350,13 +350,26 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+          ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              local entry = cmp.get_selected_entry()
+              if entry == nil then
+                fallback()
+              else
+                cmp.confirm({
+                  behavior = cmp.ConfirmBehavior.Replace,
+                  select = false,
+                })
+              end
+            else
+              fallback()
+            end
+          end, {'i', 's'}),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'nvim_lsp_signature_help' },
-          { name = 'vsnip' },
+          -- { name = 'vsnip' },
           {
             name = 'buffer',
             option = {
@@ -366,7 +379,10 @@ return {
             }
           },
         }, {
-          })
+        }),
+        completion = {
+          completeopt = 'menu,menuone,noinsert,noselect'
+        }
       })
 
       -- Set configuration for specific filetype.
