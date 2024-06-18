ca3c148d3cda3f43e66811880936aec05e499e30
nyangry <iacta.alea.esto@gmail.com>
2024-06-18 15:14:23 +0900

    Add nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -410,6 +410,55 @@ return {
     end
   },
 
+  -- snippet
+  {
+    "garymjr/nvim-snippets",
+    event = "BufRead",
+    keys = {
+      {
+        "<Tab>",
+        function()
+          if vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+            return
+          end
+          return "<Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<Tab>",
+        function()
+          vim.schedule(function()
+            vim.snippet.jump(1)
+          end)
+        end,
+        expr = true,
+        silent = true,
+        mode = "s",
+      },
+      {
+        "<S-Tab>",
+        function()
+          if vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+            return
+          end
+          return "<S-Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+    },
+  },
+
   -- It allows you to quickly select, yank, delete or replace language-specific ranges.
   {
     'David-Kunz/treesitter-unit',
