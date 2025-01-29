e29539280645a081faee1f30b1f08f76cf24607f
nyangry <iacta.alea.esto@gmail.com>
2025-01-29 23:17:58 +0900

    Add nvim-spectre

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1121,4 +1121,40 @@ return {
       },
     },
   },
+
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('spectre').setup({
+
+        color_devicons = true,
+        open_cmd = 'vnew', -- can also be a lua function
+        live_update = false, -- auto execute search again when you write to any file in vim
+        lnum_for_results = true, -- show line number for search/replace results
+        line_sep_start = '┌-----------------------------------------',
+        result_padding = '¦  ',
+        line_sep       = '└-----------------------------------------',
+        highlight = {
+          ui = "String",
+          search = "Search",
+          replace = "CurSearch"
+        },
+      })
+      vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+        desc = "Toggle Spectre"
+      })
+      vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+        desc = "Search current word"
+      })
+      vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+        desc = "Search current word"
+      })
+      vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+        desc = "Search on current file"
+      })
+    end,
+  },
 }
