caf7868f0bcd2dd461c74aca54d3f70d816993eb
nyangry <iacta.alea.esto@gmail.com>
2025-02-10 14:55:04 +0900

    Fix mapping

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -775,7 +775,7 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { ",lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      { ",g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
   }, 
 
