aa23f7023621edc32f8ccaf58bba3fb5319e1cbf
nyangry <iacta.alea.esto@gmail.com>
2025-02-12 20:24:40 +0900

    Update nvim plugin setting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -789,7 +789,7 @@ return {
       require('toggleterm').setup({
         size = function(term)
           if term.direction == "horizontal" then
-            return 15
+            return 30
           elseif term.direction == "vertical" then
             return vim.o.columns * 0.4
           end
