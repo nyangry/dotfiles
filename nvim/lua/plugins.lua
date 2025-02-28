fc9b68cb571762f976652a3353891528b574dd30
nyangry <iacta.alea.esto@gmail.com>
2025-02-28 14:25:08 +0900

    Improve performance

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -898,14 +898,14 @@ return {
         current_line_blame_opts = {
           virt_text = true,
           virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-          delay = 200,
+          delay = 1000,
           ignore_whitespace = false,
           virt_text_priority = 100,
           use_focus = true,
         },
         current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
         sign_priority = 6,
-        update_debounce = 100,
+        update_debounce = 1000,
         status_formatter = nil, -- Use default
         max_file_length = 40000, -- Disable if file is longer than this (in lines)
         preview_config = {
